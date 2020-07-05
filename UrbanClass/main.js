42ad5db6b3c19106e0253b7a586f58049572a133
tony.wangs <tony.wangs@gmail.com>
2020-07-05 19:16:09 +0000

    Updated UrbanClass/main.js (added 2 lines, deleted 1 line).

--- UrbanClass/main.js
+++ UrbanClass/main.js
@@ -1,3 +1,4 @@
 var a = require('users/tonywangs/GEOG481:UrbanClass/ImportImage.js');
 
-a.importImage()
+var result = a.importImage()
+Map.addLayer(result, {bands: ['B4', 'B3', 'B2'], gamma: 2.2});
