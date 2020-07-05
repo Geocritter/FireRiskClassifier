4a539b237a6f0c88b885a32b049fb6738c9fd6e9
tony.wangs <tony.wangs@gmail.com>
2020-07-05 19:29:42 +0000

    Updated UrbanClass/ImportImage.js (added 1 line, deleted 1 line).

--- UrbanClass/ImportImage.js
+++ UrbanClass/ImportImage.js
@@ -1,4 +1,4 @@
-exports.importImage = function(sDate, eDate) {
+exports.importImage = function(sDate, eDate, roi) {
   // Load the Landsat 8 scaled radiance image collection.
   var landsatCollection = ee.ImageCollection('LANDSAT/LC08/C01/T1')
       .filterDate(sDate, eDate);
