1ad09f2a8cd6db40473216939d61225023d42a98
tony.wangs <tony.wangs@gmail.com>
2020-07-05 19:02:48 +0000

    Updated main.js (added 1 line, deleted 1 line).

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-var n = require('users/tonywangs/GEOG481/ImportImage.js');
+var n = require('users/tonywangs/repo:GEOG481/ImportImage.js');
 
 n.importImage('2017-01-01', '2017-12-31','LANDSAT/LC08/C01/T1')
