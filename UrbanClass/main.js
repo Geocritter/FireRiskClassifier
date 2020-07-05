3c388fc9416f3bcf39fb977d961c3a1622e5c794
tony.wangs <tony.wangs@gmail.com>
2020-07-05 19:20:06 +0000

    Updated UrbanClass/main.js (added 2 lines).

--- UrbanClass/main.js
+++ UrbanClass/main.js
@@ -1,4 +1,6 @@
 var a = require('users/tonywangs/GEOG481:UrbanClass/ImportImage.js');
 
+var roi = ee.Geometry.Point(-40.5865, 122.3917);
+
 var result = a.importImage('2017-01-01', '2017-12-31')
 Map.addLayer(result, {bands: ['B4', 'B3', 'B2'], gamma: 2.2});
