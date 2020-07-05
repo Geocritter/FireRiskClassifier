9d48cf3c766fe988e1aa2a2aec5a69c4f7459e37
tony.wangs <tony.wangs@gmail.com>
2020-07-05 19:08:22 +0000

    Updated UrbanClass/main.js (added 1 line, deleted 1 line).

--- UrbanClass/main.js
+++ UrbanClass/main.js
@@ -1,4 +1,4 @@
-var a = require('users/tonywangs/repo:GEOG481/UrbanClass/ImportImage.js');\
+var a = require('users/tonywangs/repo:GEOG481/UrbanClass/ImportImage.js');
 var b = require('users/tonywangs/repo:GEOG481/UrbanClass/ImportImage.js');
 
 
