94b672b119fbd2142e6de1a802a2c59b5c369989
tony.wangs <tony.wangs@gmail.com>
2020-07-05 20:14:57 +0000

    Updated UrbanClass/ClassifyImage.js (added 11 lines, deleted 46 lines).

--- UrbanClass/ClassifyImage.js
+++ UrbanClass/ClassifyImage.js
@@ -12,40 +12,7 @@ var roi =
           [-120.99904152998705, 40.072492956391045],
           [-120.99904152998705, 41.346599408357]]], null, false);
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
-var a = require('users/tonywangs/GEOG481:UrbanClass/ImportImage.js');
-const maxYear = 2015;
-var year = 1995;
-var sDate = year.toString()+
-/*------------------------------------------------------------------------------
-                             Start Training 
--------------------------------------------------------------------------------*/
-// Select the bands for training
-var bands = ['B2', 'B3', 'B4', 'B5', 'B6', 'B7'];
-
-// Sample the input imagery to get a FeatureCollection of training data.
-var training = composite.select(bands).sampleRegions({
-  collection: newfc,
-  properties: ['landcover'],
-  scale: 30
-});
-
-// Make a Random Forest classifier and train it.
-var classifier = ee.Classifier.randomForest().train({
-  features: training,
-  classProperty: 'landcover',
-  inputProperties: bands
-});
-
-//Initiate loop start for next x amount of images
-/*------------------------------------------------------------------------------
-            Start of image loading >> classified image loading
--------------------------------------------------------------------------------*/
-var result = a.importImage('2017-05-01', '2017-12-31', roi)
-Map.addLayer(result, {bands: ['B4', 'B3', 'B2'], gamma: 2.2});
-
-
-//Classify starts here
-var classified = result.select(bands).classify(classifier);
+var classified = composite.select(bands).classify(classifier);
 
 // Define a palette for the Land Use classification.
 var palette = [
@@ -55,7 +22,5 @@ var palette = [
 ];
 
 // Display the classification result and the input image.
+Map.setCenter(-96.0171, 29.6803);
 Map.addLayer(classified, {min: 0, max: 2, palette: palette}, 'Land Use Classification');
-
-
-
