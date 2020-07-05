bb72251015cecea32777a385d4578570ab840f3d
tony.wangs <tony.wangs@gmail.com>
2020-07-05 19:17:31 +0000

    Updated UrbanClass/main.js (added 1 line, deleted 1 line).

--- UrbanClass/main.js
+++ UrbanClass/main.js
@@ -1,4 +1,4 @@
 var a = require('users/tonywangs/GEOG481:UrbanClass/ImportImage.js');
 
-var result = a.importImage()
+var result = a.importImage('2017-01-01', '2017-12-31')
 Map.addLayer(result, {bands: ['B4', 'B3', 'B2'], gamma: 2.2});
