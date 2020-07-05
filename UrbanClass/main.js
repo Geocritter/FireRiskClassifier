8b3bf2aa45d5654a35ffa9277c7bfa8e821a15ca
tony.wangs <tony.wangs@gmail.com>
2020-07-05 19:51:30 +0000

    Updated UrbanClass/main.js (added 2 lines, deleted 1 line).

--- UrbanClass/main.js
+++ UrbanClass/main.js
@@ -13,7 +13,8 @@ var roi =
           [-120.99904152998705, 41.346599408357]]], null, false);
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 var a = require('users/tonywangs/GEOG481:UrbanClass/ImportImage.js');
-
+const maxYear = 2015;
+var year = 1995;
 var result = a.importImage('2017-01-01', '2017-12-31', roi)
 
 Map.addLayer(result, {bands: ['B4', 'B3', 'B2'], gamma: 2.2});
