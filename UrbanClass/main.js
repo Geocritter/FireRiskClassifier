298b1bb2e2a18ce86bd65563956fc8c704185bab
tony.wangs <tony.wangs@gmail.com>
2020-07-05 19:36:13 +0000

    Updated UrbanClass/main.js (added 1 line, deleted 15 lines).

--- UrbanClass/main.js
+++ UrbanClass/main.js
@@ -1,17 +1,3 @@
-/**** Start of imports. If edited, may not auto-convert in the playground. ****/
-var roi = 
-    /* color: #98ff00 */
-    /* displayProperties: [
-      {
-        "type": "rectangle"
-      }
-    ] */
-    ee.Geometry.Polygon(
-        [[[-122.62222642536682, 41.14868045119151],
-          [-122.62222642536682, 40.44591041948576],
-          [-121.29013414021057, 40.44591041948576],
-          [-121.29013414021057, 41.14868045119151]]], null, false);
-/***** End of imports. If edited, may not auto-convert in the playground. *****/
 var a = require('users/tonywangs/GEOG481:UrbanClass/ImportImage.js');
 
 var result = a.importImage('2017-01-01', '2017-12-31', roi)
