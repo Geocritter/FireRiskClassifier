1762bb3ff5b5858a92559529c3d9cc7bc9f98273
tony.wangs <tony.wangs@gmail.com>
2020-07-05 20:15:22 +0000

    Updated UrbanClass/ClassifyImage.js (added 1 line, deleted 15 lines).

--- UrbanClass/ClassifyImage.js
+++ UrbanClass/ClassifyImage.js
@@ -1,17 +1,3 @@
-/**** Start of imports. If edited, may not auto-convert in the playground. ****/
-var roi = 
-    /* color: #ffc82d */
-    /* displayProperties: [
-      {
-        "type": "rectangle"
-      }
-    ] */
-    ee.Geometry.Polygon(
-        [[[-123.41054055342455, 41.346599408357],
-          [-123.41054055342455, 40.072492956391045],
-          [-120.99904152998705, 40.072492956391045],
-          [-120.99904152998705, 41.346599408357]]], null, false);
-/***** End of imports. If edited, may not auto-convert in the playground. *****/
 var classified = composite.select(bands).classify(classifier);
 
 // Define a palette for the Land Use classification.
